1f52d120aced50ed6f03ec8fc0c34d1d7cfac319
renanll <renan_ll@hotmail.com>
2021-02-08 23:22:50 -0300

    up date new page-create-orphaneges

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -17,7 +17,7 @@ const popup = L.popup({
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanege"> <img src="/nlw/public/images/arrow-white.svg" > </a>')
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanege"> <img src="images/arrow-white.svg" > </a>')
 
 //create and add marker//
 
