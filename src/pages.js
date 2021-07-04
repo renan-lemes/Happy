49f19956217b2fb03f0cde97d0504f070c80bdef
renanll <renan_ll@hotmail.com>
2021-07-03 23:16:37 -0300

    adjut the orphanage

--- src/pages.js
+++ src/pages.js
@@ -1,17 +1,43 @@
-
+const Database = require('./database/db');
+const saveOphanage = require('./database/saveOphanage');
 
 module.exports = {
     index(req,res){ //request and response//
         return res.render('index')
     },
 
-    orphanage(req,res){
-        return res.render('orphanage')
+    async orphanage(req,res){
+        
+        
+        const id = req.query.id
+
+        try {
+            const db = await Database;
+            const results = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`)
+            const orphanage = results[0]    
+            
+            orphanage.images = orphanage.images.split(",")
+            orphanage.firstImage = orphanage.images[0]
+
+            return res.render('orphanage', { orphanage })
+        } catch (error) {
+            console.log(error);
+            return res.send('Erro no banco de dados!')
+        }
+        
     },
 
-    orphanages(req,res){
+    async orphanages(req,res){
+        //colocar o orphanage pelo banco
+        try{
+            const db = await Database;
+            const orphanages = await db.all("SELECT * FROM orphanages")
+            return res.render('orphanages', { orphanages })
+        }catch(error){
+            console.log(error)
+            return res.send('Erro no banco de dados')
+        }
         
-        return res.render('orphanages', { orphanages })
     },
 
     createOphanages(req,res){
